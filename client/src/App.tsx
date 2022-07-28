b54b7885a1e9ff9f283350e35882debc25ac32db
Derek Bruckner <dbrucknr@umich.edu>
2022-07-28 09:35:50 -0400

    Add first components and relevant styles.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import './styles/App.css';
+import { QueryInput } from './components/QueryInput';
+import { QueryResult } from './components/QueryResult';
 
 const App = () => {
   return (
     <div id='main'>
-
+      <div className='layout'>
+        <div className='col'>
+          <QueryInput />
+        </div>
+        <div className='col'>
+          <QueryResult />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/QueryResult.tsx
@@ -0,0 +1,7 @@
+export const QueryResult = () => {
+    return (
+        <div id="base-query">
+
+        </div>
+    )
+}
